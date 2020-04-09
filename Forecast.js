30f05ff967dad42041545299b8fd94e4388aa69e
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-04-09 15:04:00 +0100

    Creates a Forecast module for the returning the data of a particular location using the Weatherbit Api.

---
+++ Forecast.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const Forecast = function(lat, lon, callback)
+{
+    const url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=73917db00ca5447aa7b436a55f8d750f`;
+
+    request({url: url, json: true}, (error, response) => {
+        if(error)
+        {
+            callback("Unable to connect the weather service!", undefined);
+        }
+        else if(response.body.error)
+        {
+            callback("Unable to find location. Try another search.", undefined);
+        }
+        else callback(undefined, {
+            Location: response.body.data[0].city_name,
+            Lattitude: response.body.data[0].lat,
+            Temperature: response.body.data[0].temp
+        })
+    })
+}
+
+module.exports = Forecast;
