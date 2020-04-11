7bdb8849fa4ebdc570302d9b475d0b8501a8c16a
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-04-11 13:30:31 +0100

    Serves up static assets

--- WebServer/src/App.js
+++ WebServer/src/App.js
@@ -1,26 +1,16 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+
+const publicDirectoryPath = path.join(__dirname, "../Public");
+
+app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
     res.send("Hello express");
 })
 
-app.get("/help", (req, res) => {
-    res.send([{
-        name: "Chinedu",
-        age: 27,
-        Occupation: "Software Engineer"
-    },{
-        name: "Andrew",
-        age: 24,
-        occuapation: "Tutor"
-    }]);
-})
-
-app.get("/about", (req, res) => {
-    res.send("<h1>Wanna know more about me?</h1>");
-})
 
 app.get("/weather", (req, res) => {
     res.send({
