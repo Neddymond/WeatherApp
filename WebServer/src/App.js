0d49845f466d6a82a2deaf7d72d783b530406691
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-04-11 16:18:42 +0100

    Uses handlebar to render dynamic content.

--- WebServer/src/App.js
+++ WebServer/src/App.js
@@ -1,15 +1,42 @@
 const path = require("path");
 const express = require("express");
 
+ /** Initiating express server */
 const app = express();
 
+/** Setting up extension for handlebar module */
+app.set("view engine", "hbs");
+
+/** The directory to serve static files from */
 const publicDirectoryPath = path.join(__dirname, "../Public");
 
+/** Serve up static files from the specified directory */
 app.use(express.static(publicDirectoryPath));
 
+/** Render dynamic file */
 app.get("", (req, res) => {
-    res.send("Hello express");
+    res.render("index", {
+        title: "Dynamic Template",
+        name: "Chinedu Ikechi"
+    })
 })
+
+app.get("/About", (req, res) => {
+    res.render("About", {
+        title: "About",
+        content: "Let me tell you somn you don't know about me."
+    });
+})
+
+app.get("/Help", (req, res) => {
+    res.render("Help", {
+        title: "Help",
+        message: "Come get some help."
+    })
+})
+// app.get("", (req, res) => {
+//     res.send("Hello express");
+// })
 
 
 app.get("/weather", (req, res) => {
