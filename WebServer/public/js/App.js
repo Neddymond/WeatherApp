4015b1f805865e3d2ac0400df9ccb53de7498ad6
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-04-14 15:12:34 +0100

    Wires up the user interface. Renders the searched location to the browser.

--- WebServer/public/js/App.js
+++ WebServer/public/js/App.js
@@ -2,7 +2,10 @@ console.log("Code no dey lie!");
 
 /** Get Html elements using DOM*/
 const weatherForm = document.querySelector("#searchForm");
-var locationInput = document.querySelector("input");
+const locationInput = document.querySelector("input");
+const weatherInfoHeader = document.querySelector("#weatherInfo-header");
+const message1 = document.querySelector("#message-1");
+const message2 = document.querySelector("#message-2");
 
 /** fetch weather forecast using the location from the input */
 weatherForm.addEventListener("submit", (e) => {
@@ -10,10 +13,19 @@ weatherForm.addEventListener("submit", (e) => {
 
     let inputValue = locationInput.value;
     locationInput.value = "";
+
+    /** Assign data to the html elements only when a location is provided */
+    if(!inputValue) return message1.textContent = "Please Provide a Location."
+
+    message1.textContent = `please wait while we fetch weather information for ${inputValue}`;
+    message2.textContent = "";
     
     fetch(`http://localhost:3000/weather?address=${inputValue}`).then((res) => {
         res.json().then((data) => {
-            if(data.error) return console.log('Please provide another location');
+            if(data.error) return message1.textContent = 'Please provide another location';
+            weatherInfoHeader.textContent = `${data.forecastData.Location} Current Weather Information.`;
+            message1.textContent = data.location;
+            message2.textContent = `It is currently ${data.forecastData.Temperature} degrees in ${inputValue}.`;
             console.log(data);
         })
     })
