b0f56e47054aeea7d0c5f73001d65e7a44fd714c
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-04-14 13:18:26 +0100

    Creates a search form to collect location information which will be used to fetch the weather information for that location.

--- WebServer/public/js/App.js
+++ WebServer/public/js/App.js
@@ -1,8 +1,22 @@
 console.log("Code no dey lie!");
 
-fetch('http://localhost:3000/weather?address=!').then((res) => {
-    res.json().then((data) => {
-        if(data.error) return console.log('Please provide another location');
-        console.log(data);
+/** Get Html elements using DOM*/
+const weatherForm = document.querySelector("#searchForm");
+var locationInput = document.querySelector("input");
+
+/** fetch weather forecast using the location from the input */
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let inputValue = locationInput.value;
+    locationInput.value = "";
+    
+    fetch(`http://localhost:3000/weather?address=${inputValue}`).then((res) => {
+        res.json().then((data) => {
+            if(data.error) return console.log('Please provide another location');
+            console.log(data);
+        })
     })
+
+    // console.log(inputValue);
 })
