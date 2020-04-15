548ca5cc662822cb9a86de5674ee4900b4f734ad
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-04-15 14:32:24 +0100

    Changes file directories

--- WebServer/src/App.js
+++ WebServer/src/App.js
@@ -8,7 +8,7 @@ const forecast = require("./Utils/Forecast");
 const app = express();
 
 /** Port for starting up the App; Heroku port or localhost */
-const part = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 /** Define paths for Express config */
 const publicDirectoryPath = path.join(__dirname, "../public");
